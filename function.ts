918c1b530512fb97e00b07ce3504c0658626dedd
debel damilola <debeldamilola@gmail.com>
2020-11-19 14:36:22 +0100

    function annotation and inference

---
+++ function.ts
@@ -0,0 +1,11 @@
+const add = (a: number, b: number) => a + b;
+
+const sub = (a: number, b: number): number => a - b;
+
+function divide(a: number, b: number): number {
+  return a / b;
+}
+
+const mult = function (a: number, b: number): number {
+  return a * b;
+};
