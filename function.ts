fdf3b3c91d06d405833883e13e3ba4bf82cfe38e
debel damilola <debeldamilola@gmail.com>
2020-11-19 14:43:32 +0100

    return void and never

--- function.ts
+++ function.ts
@@ -9,3 +9,12 @@ function divide(a: number, b: number): number {
 const mult = function (a: number, b: number): number {
   return a * b;
 };
+
+const logger = (message: string): void => {
+  console.log(message);
+};
+
+//we only return never when we are sure we never going to reach the end og the function
+const throwErr = (message: string): never => {
+  throw new Error(message);
+};
