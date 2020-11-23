2e6540bcd0e77a61b8b3e6982c35b6cd7061673d
debel damilola <debeldamilola@gmail.com>
2020-11-23 22:40:20 +0100

    tuples

---
+++ tuples.ts
@@ -0,0 +1,10 @@
+const drink = {
+  color: 'brown',
+  carbonated: true,
+  sugar: 30,
+};
+type Drink = [string, boolean, number];
+
+const pepsi: Drink = ['brown', true, 30];
+const tea: Drink = ['light-brown', true, 0];
+// tuple[0] = true;
